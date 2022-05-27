83310d0c2604be3a1d98bf2bec7685b709849725
sergiy.matveyev <sergiy.matveyev@vakoms.com>
2022-05-28 00:22:45 +0300

    Modified routes system

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,10 +1,14 @@
 "use strict";
 
 const express = require("express");
+const apiRouter = express.Router();
+
 const bookRoutes = require("./book_routes.js");
 const bookRouter = express.Router();
 
 module.exports = function (app) {
-  app.use("/api/v1/books", bookRouter);
+  app.use("/api/v1", apiRouter);
+
+  apiRouter.use("/books", bookRouter);
   bookRoutes(bookRouter);
 };
